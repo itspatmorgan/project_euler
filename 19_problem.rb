7bd377a0e5a6a49bb5f2606445c4b652f88d24e8
Patrick Morgan <patrick.m.morgan@aexp.com>
2015-03-04 17:46:17 -0500

    added some thoughts on 19

--- 19_problem.rb
+++ 19_problem.rb
@@ -12,3 +12,41 @@
 # How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
 # 01-01-1901 was a Tuesday
+# First sunday was 01-06-1901
+
+# only want to check 1sts of months (1200 max)
+# increment by number of days in month
+
+-5 + increment % 7
+
+century = (1901..2000)
+
+sundays = 0
+
+year = 365
+leap_year = 366
+
+yr = {
+	jan: 31,
+	feb: 28,
+	mar: 31,
+	apr: 30,
+	may: 31,
+	jun: 30,
+	jul: 31,
+	aug: 31,
+	sep: 30,
+	oct: 31,
+	nov: 30,
+	dec: 31
+}
+
+for year in century do
+	if year % 4 == 0
+		yr[:jan] = 29
+
+
+	else 
+		yr
+	end
+end
